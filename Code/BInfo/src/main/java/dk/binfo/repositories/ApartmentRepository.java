ae74a63805bb3abfc670c5df6ab91358bcdfd836
Jens Bäckvall <jensbackvall@t-nova.org>
2017-11-27 11:03:56 +0100

    move settings + edit comment

--- Code/BInfo/src/main/java/dk/binfo/repositories/ApartmentRepository.java
+++ Code/BInfo/src/main/java/dk/binfo/repositories/ApartmentRepository.java
@@ -12,8 +12,14 @@ public interface ApartmentRepository extends JpaRepository<Apartment, Integer> {
 
 
 /*
-In the early days you now had to create a DAO class which has methods for creating, updating, deleting and reading data from the table. With Spring Data on the other hand all you need is an interface that extends another interface. For example:
+In the early days you now had to create a DAO class which has methods for creating, updating,
+deleting and reading data from the table. With Spring Data on the other hand all you need is
+an interface that extends another interface. For example:
 
- So in this case we created an interface called ApartmentRepository, extending Spring’s JpaRepository, providing some generics for the entity and the type of the ID, being Apartment and Long. The reason we have these generics is so the return type and parameter types of the methods can be determined. For example, the findOne() method should accept a parameter of type Long and should return an entity of type Apartment.
+ So in this case we created an interface called ApartmentRepository, extending Spring’s
+ JpaRepository, providing some generics for the entity and the type of the ID, being
+ Apartment and Long. The reason we have these generics is so the return type and parameter
+ types of the methods can be determined. For example, the findOne() method should accept a
+ parameter of type Long and should return an entity of type Apartment.
 
  */
