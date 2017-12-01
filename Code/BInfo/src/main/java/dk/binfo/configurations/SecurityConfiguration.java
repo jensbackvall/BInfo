275c02f5a22203236fbbff6ed94882a210c2a5ca
Patrick Klæbel <Vagabonden@outlook.com>
2017-12-01 13:21:09 +0100

    Lavet users og users/add

--- Code/BInfo/src/main/java/dk/binfo/configurations/SecurityConfiguration.java
+++ Code/BInfo/src/main/java/dk/binfo/configurations/SecurityConfiguration.java
@@ -43,6 +43,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.antMatchers("/").permitAll()
 				.antMatchers("/login").permitAll()
 				.antMatchers("/apartment/**").hasAuthority("ADMIN")
+				.antMatchers("/users/**").hasAuthority("ADMIN")
 				.antMatchers("/registration").permitAll()
 				.antMatchers("/error").permitAll()
 				// TODO admin skal kunne se alle ventelister (/admin/list/Family/intern/ekstern/connectMrawesomeSexy)
